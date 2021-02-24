3499f4ba936909d082184a3f25db4c5899e26d95
Austin Woodall <austin.woodall72@gmail.com>
2021-02-24 16:45:17 -0600

    initialize all reducers

--- Redux/reducers/index.ts
+++ Redux/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import locationsReducer from "./locations";
 import currentLocationReducer from "./currentLocation";
+import weatherReducer from "./weatherReducer";
 
 export default combineReducers({
 	locations: locationsReducer,
-	currentLocation: currentLocationReducer
+	currentLocation: currentLocationReducer,
+	weather: weatherReducer
 });
