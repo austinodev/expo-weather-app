3ceaf8c1fa71c492ef1fc7b10a298ff2ae1900ae
Austin Woodall <austin.woodall72@gmail.com>
2021-02-24 16:44:45 -0600

    home screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,27 +1,24 @@
 import React, { useEffect } from "react";
 import { View, Text } from "react-native";
 import tailwind from "tailwind-rn";
-import { useDispatch } from "react-redux";
-import getWeatherData from "../Redux/actions/getWeatherData";
-import useUserLocation from "../hooks/checkUserLocation";
-import useGetWeather from "../hooks/useGetWeather";
+import { useSelector } from "react-redux";
+import useTodayForecast from "../hooks/useTodayForecast";
+import DailyForecast from "../components/DailyForecast";
+import CurrentWeather from "../components/CurrentWeather";
 
 export default function Home() {
-	const dispatch = useDispatch();
-	const { isLocationEnabled, latitude, longitude } = useUserLocation();
-	const { data, loading } = useGetWeather(latitude, longitude);
-
-	useEffect(() => {
-		console.log(data);
-	}, [isLocationEnabled, longitude, latitude]);
+	const location = useSelector(
+		({ currentLocation }: any) =>
+			currentLocation?.name + ", " + currentLocation?.state
+	);
 
 	return (
-		<View style={tailwind("flex-1 pt-16")}>
-			<View>
-				<Text>
-					Location enabled: {isLocationEnabled ? "True" : "False"}
-				</Text>
+		<View style={tailwind("flex-1 pt-20")}>
+			<View style={tailwind("px-6 pb-8")}>
+				<Text style={tailwind("text-2xl")}>{location}</Text>
 			</View>
+			<CurrentWeather />
+			<DailyForecast />
 		</View>
 	);
 }
